cdbcab3d2364ce654f89d60960f65b56f5e75135
williamtesche <85637800+williamtesche@users.noreply.github.com>
2021-08-12 13:45:39 -0300

    .

--- bootstrap.php
+++ bootstrap.php
@@ -7,16 +7,16 @@ error_reporting(E_ALL);
 
 // conecta no banco
 $host =  "localhost:8889";
-$user = "user"; 
-$pass = "123";
-$base = "empresa";
+$user = "william_willian"; 
+$pass = "v&h]BtTAAAvZ";
+$base = "william_crud";
 
-$conn = mysqli_connect($host,$user,$pass,$base);
+$conn = mysqli_connect($host,$base,$user,$pass);
 
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-echo "Connected successfully";
+// if (!$conn) {
+//     die("Connection failed: " . mysqli_connect_error());
+// }
+// echo "Connected successfully";
 ?>
 
 
